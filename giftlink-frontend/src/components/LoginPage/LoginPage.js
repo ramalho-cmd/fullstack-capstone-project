8a8429a71b3a3495f208d62918075ca721d33aaa
ramalho-cmd <rapharam96@gmail.com>
2025-06-09 15:26:39 -0400

    Lab4 module 4

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
 
@@ -7,40 +10,96 @@ function LoginPage() {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
- 
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
+
     const handleLogin = async () => {
-        console.log("Dentro do handleLogin");
+        try{
+        //first task
+      const response = await fetch(`/api/auth/login`, {
+           method : 'POST',
+           headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+          },
+          body: JSON.stringify({    
+            email: email,
+            password: password,
+          })
+          
+     })
+     const json = await res.json();
+     console.log('Json',json);
+     if (json.authtoken) {
+         //Step 2: Task 2
+       sessionStorage.setItem('auth-token', json.authtoken);
+       sessionStorage.setItem('name', json.userName);
+       sessionStorage.setItem('email', json.userEmail);
+         //Step 2: Task 3
+       setIsLoggedIn(true);
+         //Step 2: Task 4
+       navigate('/app');
+     } else {
+         //Step 2: Task 5
+       document.getElementById("email").value="";
+       document.getElementById("password").value="";
+       setIncorrect("Wrong password. Try again.");
+       setTimeout(() => {
+         setIncorrect("");
+       }, 2000);
+     }
+   
+      }catch (e) {
+        console.log("Error fetching details: " + e.message);
+    }
     }
 
         return (
-      <div className="container mt-5">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-lg-4">
-            <div className="login-card p-4 border rounded">
-              <h2 className="text-center mb-4 font-weight-bold">Login</h2>
-
-              <div className="mb-4">
-                <label htmlFor="firstName" className="form label"> FirstName</label>
-                <input
-                id="firstName"
-                type="text"
-                className="form-control"
-                placeholder="Enter your firstName"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                />
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-lg-4">
+                        <div className="login-card p-4 border rounded">
+                            <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input
+                                    id="email"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter your email"
+                                    value={email}
+                                    onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Enter your password"
+                                    value={password}
+                                    onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
+                                />
+                                
+                                <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+                            </div>
+                            <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
+                            <p className="mt-4 text-center">
+                                New here? <a href="/app/register" className="text-primary">Register Here</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
-                <p className="mt-4 text-center">
-                    Novo aqui? <a href="/app/register" className="text-primary">Registre-se Aqui</a>
-                </p>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+        );
 }
 
 export default LoginPage;
