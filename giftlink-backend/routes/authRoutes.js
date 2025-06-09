cb1adce1a303492fe2d0644c349e561899917965
ramalho-cmd <rapharam96@gmail.com>
2025-06-09 16:17:41 -0400

    Lab5 module 4

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -7,6 +7,7 @@ const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
+const { body, validationResult } = require('express-validator');
 
 const logger = pino();  // Create a Pino logger instance
 
@@ -78,6 +79,51 @@ router.post('/login', async (req, res) => {
     }
 });
 
+
+// {Insert it along with other imports} Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
+
+router.put('/update', async (req, res) => {
+    const errors = validationResult(req);
+if (!errors.isEmpty()) {
+  logger.error('Validation errors in update request', errors.array());
+  return res.status(400).json({ errors: errors.array() });
+}
+try {
+    const email = req.headers.email;
+
+    if (!email) {
+        logger.error('Email not found in the request headers');
+        return res.status(400).json({ error: "Email not found in the request headers" });
+    }
+    const db = await connectToDatabase();
+    const collection = db.collection("users");  
+    const existingUser = await collection.findOne({ email });
+
+    if (!existingUser) {
+        logger.error('User not found');
+        return res.status(404).json({ error: "User not found" });
+    }
+    existingUser.firstName = req.body.name;
+    existingUser.updatedAt = new Date();
+    
+    const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: existingUser },
+        { returnDocument: 'after' }
+    );
+    const payload = {
+        user: {
+            id: updatedUser._id.toString(),
+        },
+    };
+
+const authtoken = jwt.sign(payload, JWT_SECRET);
+    res.json({authtoken});
+} catch (e) {
+     return res.status(500).send('Internal server error');
+
+}
+});
 
 
 module.exports = router;
