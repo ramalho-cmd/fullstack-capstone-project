0a4e39b70354141f8d3076a4d61bf3c91d6748c5
ramalho-cmd <rapharam96@gmail.com>
2025-06-10 11:51:13 -0300

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route/*, useNavigate*/ } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -10,7 +10,7 @@ import SearchPage from './components/SearchPage/SearchPage';
 import Profile from './components/Profile/Profile';
 
 function App() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   return (
         <>
         <Navbar/>
