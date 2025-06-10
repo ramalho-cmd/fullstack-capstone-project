3d0a49404f20a0812aedf8c7b9483eab84e98793
ramalho-cmd <rapharam96@gmail.com>
2025-06-10 10:52:31 -0300

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require("./routes/giftRoutes")
+const giftRoutes = require("./routes/giftRoutes");
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
@@ -51,8 +51,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
